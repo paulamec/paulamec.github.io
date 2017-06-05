d9720a5ef7033c0e2adcec898ac1116677671abb
paulamec <paulaarmc@hotmail.com>
2017-06-05 20:17:07 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+jQuery(document).ready(function() {
+	$('#fullpage').fullpage({
+		sectionsColor: ['#cfd8db', '#ffffff', '#dde0e2', 'whitesmoke', '#ccddff'],
+		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+		menu: '#menu',
+		scrollingSpeed: 1000
+	});
+});
+
+
+
