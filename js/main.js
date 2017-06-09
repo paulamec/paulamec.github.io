0af83b2ba10978da745d25eaf08b990ca8ae3208
paulamec <paulaarmc@hotmail.com>
2017-06-09 10:11:57 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 jQuery(document).ready(function() {
 	$('#fullpage').fullpage({
 		sectionsColor: ['#cfd8db', '#ffffff', '#dde0e2', 'whitesmoke', '#ccddff'],
-		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', '5thpage'],
 		menu: '#menu',
 		scrollingSpeed: 1000
 	});
